e37d9e6eb514b21e2a9688f8113a40a64363d576
Victor Neves <victor.neves4@fatec.sp.gov.br>
2021-08-04 14:07:22 -0300

    :sparkles: add new components to the app page Changes to be committed: 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import FirstComponent from "./components/FirstComponent";
+import SecondComponent from "./components/SecondComponent";
+import ThirdComponent from "./components/ThirdComponent";
+import FourthComponent from "./components/FourthComponent";
+import { SimpleContextProvider } from "./state/FirstExample-SimpleContext";
+import { ComplexContextProvider } from "./state/SecondExample-ComplexContext";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <SimpleContextProvider>
+        <div style={{ width: "100%" }}>
+          <FirstComponent />
+          <SecondComponent />
+        </div>
+      </SimpleContextProvider>
+      <br />
+      <ComplexContextProvider>
+        <div style={{ width: "100%" }}>
+          <ThirdComponent />
+          <FourthComponent />
+        </div>
+      </ComplexContextProvider>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
