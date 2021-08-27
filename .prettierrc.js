d25d099a63d512e74a243d4e75598a63ae1a5e20
Victor Neves <victor.neves4@fatec.sp.gov.br>
2021-08-27 11:53:21 -0300

    :wrench: add prettier config file Changes to be committed: 	new file:   .prettierrc.js

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  tabWidth: 2,
+  singleQuote: true,
+  trailingComma: 'none',
+  semi: false,
+  printWidth: 80,
+  arrowParens: 'always',
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  quoteProps: 'as-needed',
+  useTabs: false
+}
